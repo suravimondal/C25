cf3caaf53bb26b86fd60c8551411e0428ed48905
suravimondal <73650143+suravimondal@users.noreply.github.com>
2021-06-02 05:53:50 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,16 @@
+
+// extends is used to create a child class
+
+class Bird extends BaseClass{
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image= loadImage("sprites/bird.png");
+    }
+    display(){
+      this.body.position.x= mouseX;
+      this.body.position.y= mouseY;
+      super.display();
+    }
+
+  }
+  
